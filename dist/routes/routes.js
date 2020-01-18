accc45688ff54ab5591d7f4a18f0509a734b1c9d
Wasauskyok <daniechoque159@gmail.com>
2020-01-18 01:50:20 -0500

    add Object.assign

--- dist/routes/routes.js
+++ dist/routes/routes.js
@@ -7,11 +7,17 @@ exports["default"] = void 0;
 
 var _express = require("express");
 
+var _path = _interopRequireDefault(require("path"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
 var router = (0, _express.Router)();
 router.post('/file', function (req, resp, next) {
-  return resp.send({
+  return resp.send(Object.assign({
     path: req.file
-  });
+  }, {
+    upload: _path["default"].join(__dirname, '../uploads/images')
+  }));
 });
 router.get('/go', function (req, res) {
   return res.send({
